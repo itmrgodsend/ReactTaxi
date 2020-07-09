6482fdc3c6df713558b79a07675626d2fe867cb2
itmrgodsend <itmrgodsend@gmail.com>
2020-07-09 03:22:15 +0300

    Add Routing

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {AuthProvider} from "./AuthContext";
+import {BrowserRouter} from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
+      <BrowserRouter>
       <AuthProvider>
     <App />
       </AuthProvider>
+          </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
